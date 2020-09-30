84e72a87b9f1a37d098f0a2c6ed00413e4e01152
Michele Campeotto <micampe@micampe.it>
2020-09-30 22:33:14 +0200

    added restart command

--- src/debug.cpp
+++ src/debug.cpp
@@ -2,10 +2,24 @@
 
 xLogger Debug;
 
+const char* commandsDesc = "Command restart restarts ESP.";
+
+bool handleCommands(String &cmd) {
+    if (cmd == "restart") {
+        MIE_LOG("restarting...");
+        delay(1000);
+        ESP.restart();
+        return true;
+    }
+
+    return false;
+}
+
 void setupRemoteDebug(const char ssid[]) {
     Debug.begin(ssid);
     Debug.setProgramVersion((char *)GIT_DESCRIBE);
     Debug.setShowDebugLevel(false);
     Debug.setTimeFormat(ltNone);
+    Debug.cmdCallback(handleCommands, commandsDesc);
     MIE_LOG("%s connected", ssid);
 }
