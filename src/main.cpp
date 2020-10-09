f5dd063b820fb7c53b7257f3ad923d171268299b
Michele Campeotto <micampe@micampe.it>
2020-10-09 19:41:39 +0200

    moved wifi init to a separate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,19 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
-#include <DoubleResetDetect.h>
-#include <WiFiManager.h>
 #include <arduino_homekit_server.h>
 #include <led_status.h>
 
-#include "accessory.h"
 #include "debug.h"
 #include "heatpump_client.h"
 #include "homekit.h"
 #include "humidity.h"
 #include "led_status_patterns.h"
 #include "ntp_clock.h"
+#include "wifi_manager.h"
 
 #define NAME_PREFIX "Heat Pump "
 
-#define DRD_TIMEOUT 2.0
-#define DRD_ADDRESS 0x00
-DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
-
 char ssid[25];
-
-void wifiConnectionFailed() {
-    led_status_signal(&status_led_error);
-    Serial.println("WiFi connection failed, restarting");
-    delay(1000);
-    ESP.reset();
-    delay(3000);
-}
-
-void wifiConfigModeCallback(WiFiManager *wifiManager) {
-    led_status_set(&status_led_waiting_wifi);
-}
 
 void setup() {
     Serial.begin(115200);
@@ -41,29 +23,7 @@ void setup() {
 
     sprintf(ssid, NAME_PREFIX "%06x", ESP.getChipId());
 
-    WiFi.setSleepMode(WIFI_NONE_SLEEP);
-
-    WiFiManager wifiManager;
-    wifiManager.setAPCallback(wifiConfigModeCallback);
-    wifiManager.setTimeout(120);
-
-    if (drd.detect()) {
-        Serial.println("Double reset detected");
-        led_status_signal(&status_led_double_reset);
-
-        Serial.println("Clearing HomeKit config");
-        homekit_storage_reset();
-
-        Serial.println("Starting config portal");
-        if (!wifiManager.startConfigPortal(ssid)) {
-            wifiConnectionFailed();
-        }
-    } else {
-        if (!wifiManager.autoConnect(ssid)) {
-            wifiConnectionFailed();
-        }
-    }
-
+    initWiFiManager(ssid);
     initNTPClock();
     initRemoteDebug(ssid);
     MIE_LOG("Initializing OTA...");
---
+++ src/wifi_manager.cpp
@@ -0,0 +1,52 @@
+#include "wifi_manager.h"
+
+#include <ArduinoOTA.h>
+#include <DoubleResetDetect.h>
+#include <WiFiManager.h>
+#include <arduino_homekit_server.h>
+#include <led_status.h>
+
+#include "led_status_patterns.h"
+
+
+#define DRD_TIMEOUT 2.0
+#define DRD_ADDRESS 0x00
+static DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
+
+
+static void wifiConnectionFailed() {
+    led_status_signal(&status_led_error);
+    Serial.println("WiFi connection failed, restarting");
+    delay(1000);
+    ESP.reset();
+    delay(3000);
+}
+
+static void wifiConfigModeCallback(WiFiManager *wifiManager) {
+    led_status_set(&status_led_waiting_wifi);
+}
+
+void initWiFiManager(const char* ssid) {
+    WiFi.setSleepMode(WIFI_NONE_SLEEP);
+
+    WiFiManager wifiManager;
+    wifiManager.setAPCallback(wifiConfigModeCallback);
+    wifiManager.setTimeout(120);
+
+    if (drd.detect()) {
+        Serial.println("Double reset detected");
+        led_status_signal(&status_led_double_reset);
+
+        Serial.println("Clearing HomeKit config");
+        homekit_storage_reset();
+
+        Serial.println("Starting config portal");
+        if (!wifiManager.startConfigPortal(ssid)) {
+            wifiConnectionFailed();
+        }
+    } else {
+        if (!wifiManager.autoConnect(ssid)) {
+            wifiConnectionFailed();
+        }
+    }
+}
