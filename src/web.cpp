ba9bea23a2f21ba72ba68581a367188e10156767
Michele Campeotto <micampe@micampe.it>
2020-11-01 18:35:40 +0100

    delete config file if deserialization fails

--- src/web.cpp
+++ src/web.cpp
@@ -96,14 +96,16 @@ void settings_init() {
     DeserializationError error = deserializeJson(doc, f);
     if (error) {
         MIE_LOG("Error loading configuration file");
-    } else {
-        uint16_t port = strtol(doc["mqtt_port"] | "", nullptr, 10);
-        settings.mqtt_port = port > 0 ? port : 1883;
-        strlcpy(settings.mqtt_server, doc["mqtt_server"] | "", sizeof(settings.mqtt_server));
-        strlcpy(settings.mqtt_temp, doc["mqtt_temp"] | "", sizeof(settings.mqtt_temp));
-        strlcpy(settings.mqtt_humidity, doc["mqtt_hum"] | "", sizeof(settings.mqtt_humidity));
-        strlcpy(settings.mqtt_dew_point, doc["mqtt_dew_point"] | "", sizeof(settings.mqtt_dew_point));
+        LittleFS.remove(CONFIG_FILE);
     }
+
+    uint16_t port = strtol(doc["mqtt_port"] | "", nullptr, 10);
+    settings.mqtt_port = port > 0 ? port : 1883;
+    strlcpy(settings.mqtt_server, doc["mqtt_server"] | "", sizeof(settings.mqtt_server));
+    strlcpy(settings.mqtt_temp, doc["mqtt_temp"] | "", sizeof(settings.mqtt_temp));
+    strlcpy(settings.mqtt_humidity, doc["mqtt_hum"] | "", sizeof(settings.mqtt_humidity));
+    strlcpy(settings.mqtt_dew_point, doc["mqtt_dew_point"] | "", sizeof(settings.mqtt_dew_point));
+
     f.close();
 }
 
